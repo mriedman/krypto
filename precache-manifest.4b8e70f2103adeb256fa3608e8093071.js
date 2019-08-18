b32544daef658684bb495362af87ab4d63ef0e28
mriedman <33470702+mriedman@users.noreply.github.com>
2019-08-18 15:38:56 -0400

    Add files via upload

---
+++ precache-manifest.4b8e70f2103adeb256fa3608e8093071.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03e1e33a20e513ef12fe9de2a09b59ff",
+    "url": "/krypto/index.html"
+  },
+  {
+    "revision": "3e687693e10c7a181174",
+    "url": "/krypto/static/css/main.c0ce03c2.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/krypto/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "3e687693e10c7a181174",
+    "url": "/krypto/static/js/main.549a89e6.chunk.js"
+  },
+  {
+    "revision": "27a950f153b96c5b2ec8",
+    "url": "/krypto/static/js/runtime~main.532c3031.js"
+  }
+]);
